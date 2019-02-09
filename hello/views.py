7045b7a1d74005fcf21d9769d8d8aa2b938d4534
Joakim Johansson <40245316+lakritslime@users.noreply.github.com>
2019-02-09 17:38:16 +0100

    Second commit

--- hello/views.py
+++ hello/views.py
@@ -8,10 +8,10 @@ from .models import Greeting
 # Create your views here.
 def index(request):
 	r = requests.get('http://httpbin.org/status/418')
-	print(r.text)
+	#print(r.text)
 
-	return HttpResponse('<pre>' + r.text + '</pre>')
-	#return render(request, "index.html")
+	#return HttpResponse('<pre>' + r.text + '</pre>')
+	return render(request, "index.html", {"r": r})
 
 
 def db(request):
